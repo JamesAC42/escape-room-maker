4807d08e06b23df5d47cf3cc24bc108a3f6d88a3
James Crovo <jamescrovo2019@lschs.org>
2021-06-14 22:42:15 -0400

    Add db and redis connections

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const redis = require('redis');
+const session = require('express-session');
+const pg = require('pg');
+
+const conInfo = require('../pw.json');
+const config = require('../config.json');
+
+const RedisStore = require('connect-redis')(session);
+const redisClient = redis.createClient();
+
+const conString = `postgres://${conInfo.username}:${conInfo.password}@localhost:5432/escaperoom`;
+const client = new pg.Client(conString);
+client.connect();
 
 const app = express();
 
@@ -11,6 +23,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 import testReq from './controllers/test';
+
+app.set('trust proxy', 1);
+app.use(session({
+    secret: 'rpiengineers',
+    resave: true,
+    name: 'escapesesh',
+    proxy:true,
+    saveUninitialized: true,
+    cookie: { secure:config.secureSession },
+    store: new RedisStore({ client: redisClient })
+}));
 
 app.get('/api/testReq', (req:any, res:any) => {
     testReq(req, res);
