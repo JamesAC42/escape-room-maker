55942d248c4462fbfd5ee8dfc43713a8080878f6
litala123 <litala@rpi.edu>
2021-06-21 10:36:37 -0400

    Added sql for creating Maps table

---
+++ src/sql/CREATE_MAPS.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS public."Maps"
+(
+    uid uuid NOT NULL,
+    creator uuid NOT NULL,
+    created_on timestamp without time zone NOT NULL,
+    last_modified timestamp without time zone NOT NULL,
+    ratings text COLLATE pg_catalog."default" NOT NULL,
+    time_limit integer NOT NULL DEFAULT 0,
+    tags text COLLATE pg_catalog."default" NOT NULL,
+    description text COLLATE pg_catalog."default" NOT NULL,
+    title character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    explicit boolean NOT NULL,
+    times_completed integer NOT NULL DEFAULT 0,
+    graph text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT "Maps_pkey" PRIMARY KEY (uid)
+)
+WITH (
+    OIDS = FALSE
+)
