ca5ba8a90751de7444136a7addf65f392fedf6cd
Samantha Neumann <sm.neumann@yahoo.com>
2021-06-24 19:47:51 -0400

    made the basic component window and associated css and path

---
+++ front/src/js/components/Window.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../../css/Window.scss';
+
+class Window extends Component {
+  render() {
+    let headText = this.props.headText;
+    // maybe have text and images and stuff?
+    // maybe have a way to change which?
+    return(
+      <div className="window">
+        
+      </div>
+    )
+  }
+}
+
+export default Window;
