52c56b558bbe48ad36f5024d08c82c07003bafe8
James Crovo <jamescrovo2019@lschs.org>
2021-06-24 22:15:33 -0400

    Fix import path

--- front/src/js/App.js
+++ front/src/js/App.js
@@ -8,7 +8,7 @@ import {
 } from 'react-router-dom';
 
 import Home from './components/Home';
-import Create from './components/Create';
+import Create from './components/create/Create';
 import Window from './components/Window';
 
 function App() {
