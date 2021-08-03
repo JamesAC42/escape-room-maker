0fc78bbc1b8cae351be52e04ea7ca6fe7c42fb1e
Samantha Neumann <sm.neumann@yahoo.com>
2021-08-03 13:26:16 -0400

    getUsername

--- src/dbActions/getUsername.ts
+++ src/dbActions/getUsername.ts
@@ -1,9 +1,13 @@
+/*
+  retrieves the username of an account from a given user id
+ */
+
 import userQueries from "../queries/userQueries"
 
 const getUsername = (db:any, userid:string):Promise<string> => {
 
     return new Promise((resolve:any, reject:any) => {
-        const query = {
+        const query = { // get the username from the database
             name: 'get-username',
             text: userQueries.getUsername,
             values: [userid]
@@ -11,10 +15,10 @@ const getUsername = (db:any, userid:string):Promise<string> => {
     
         db.query(query)
         .then((r:any) => {
-            if(r.rows.length === 0) {
-                reject("Invalid user id");
-            } else {
-                resolve(r.rows[0].username);
+            if(r.rows.length === 0) { // if there is no user associated with the account...
+                reject("Invalid user id"); // tell the system that the id is invalid
+            } else { // otherwise...
+                resolve(r.rows[0].username); // send the username
             }
         })
     })
