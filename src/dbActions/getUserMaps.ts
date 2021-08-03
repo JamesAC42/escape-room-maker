7cb89a8a5eb401ff7583cc62e602a28f7a3e40ef
James Crovo <jamescrovo2019@lschs.org>
2021-08-02 23:24:46 -0400

    Fix error being thrown when no maps are found for a user

--- src/dbActions/getUserMaps.ts
+++ src/dbActions/getUserMaps.ts
@@ -11,23 +11,19 @@ const getUserMaps = (db:any, userid:string):Promise<Array<any>> => {
             text: mapQueries.getUserMaps,
             values: [userid]
         };
-    
+
         // Perform the query
         db.query(query)
         .then((r:any) => {
-            if(r.rows.length === 0) {
-                reject("Invalid user id");
-            } else {
-                // Parse the JSON for the tags and ratings list stored in each map
-                r.rows.forEach((map:any) => {
-                    map.tags = JSON.parse(map.tags);
-                    map.createdOn = map.created_on;
-                    delete map.created_on;
-                    map.ratings = JSON.parse(map.ratings);
-                });
-                // Send the data back to the caller
-                resolve(r.rows);
-            }
+            // Parse the JSON for the tags and ratings list stored in each map
+            r.rows.forEach((map:any) => {
+                map.tags = JSON.parse(map.tags);
+                map.createdOn = map.created_on;
+                delete map.created_on;
+                map.ratings = JSON.parse(map.ratings);
+            });
+            // Send the data back to the caller
+            resolve(r.rows);
         })
         .catch((err:any) => {
             reject("Error querying database");
