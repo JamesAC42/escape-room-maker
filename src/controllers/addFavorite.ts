07acf8ed9618caaeabfcc5eefc85be2e66f8edaf
Samantha Neumann <sm.neumann@yahoo.com>
2021-08-03 19:15:07 -0400

    did some formatting

--- src/controllers/addFavorite.ts
+++ src/controllers/addFavorite.ts
@@ -1,73 +1,64 @@
-import getUserFavorites from "../dbActions/getUserFavorites";
-import setUserFavorites from "../dbActions/setUserFavorites";
-import { IMap } from "../interfaces/IMap";
-import getMap from '../dbActions/getMap';
+import getUserFavorites from '../dbActions/getUserFavorites'
+import setUserFavorites from '../dbActions/setUserFavorites'
+import { IMap } from '../interfaces/IMap'
+import getMap from '../dbActions/getMap'
 
 // API controller for adding a map to a user's favorites list.
 // Takes a req and res object, and a database object.
 // The map id to be added is in the req body
 const addFavorite = (req:any, res:any, db:any) => {
+  // Get the map id from the req body
+  const {
+    mapid
+  } = req.body
 
-    // Get the map id from the req body
-    const {
-        mapid
-    } = req.body;
+  // Make sure the id was provided
+  if (mapid === undefined) {
+    res.send({ success: false, message: 'Invalid schema' })
+    return
+  }
 
-    // Make sure the id was provided
-    if(mapid === undefined) {
-        res.send({success:false, message:"Invalid schema"});
-        return;
-    }
+  // Ensure the client has a valid session, get the user id
+  const userId = req.session.key
+  if (userId === undefined) {
+    res.send({ success: false, message: 'Invalid session' })
+    return
+  }
 
-    // Ensure the client has a valid session, get the user id
-    const userId = req.session.key;
-    if(userId === undefined) {
-        res.send({success:false, message:"Invalid session"});
-        return;
-    }
-    
-    // Ensure the map id is a valid uid
-    if(!/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(mapid)) {
-        res.send({success:false, message:"Invalid schema"});
-        return;
-    }
+  // Ensure the map id is a valid uid
+  if (!/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(mapid)) {
+    res.send({ success: false, message: 'Invalid schema' })
+    return
+  }
 
-    // Get the user's favorites from the database
-    getUserFavorites(db, userId)
+  // Get the user's favorites from the database
+  getUserFavorites(db, userId)
     .then((favorites:Array<string>) => {
-
-        // Get the map information of the given map id
-        getMap(db, mapid)
+      // Get the map information of the given map id
+      getMap(db, mapid)
         .then((map:IMap) => {
+          // Add the map id to the list of favorites
+          favorites.push(mapid)
 
-            // Add the map id to the list of favorites
-            favorites.push(mapid);
-
-            // Set the new favorites list back into the database
-            setUserFavorites(db, userId, favorites)
+          // Set the new favorites list back into the database
+          setUserFavorites(db, userId, favorites)
             .then(() => {
-
-                // Send the map data back to the client
-                res.send({
-                    success:true,
-                    map
-                });
-                return;
+              // Send the map data back to the client
+              res.send({
+                success: true,
+                map
+              })
             })
             .catch((err:Error) => {
-                console.error(err);
-                res.send({success:false});
-            });
+              console.error(err)
+              res.send({ success: false })
+            })
         })
         .catch((err:Error) => {
-            console.error(err);
-            res.send({success:false});
+          console.error(err)
+          res.send({ success: false })
         })
-
-
     })
-
-
 }
 
-export default addFavorite;
+export default addFavorite
--- src/dbActions/getUserMaps.ts
+++ src/dbActions/getUserMaps.ts
@@ -1,38 +1,36 @@
-import mapQueries from "../queries/mapQueries";
+import mapQueries from '../queries/mapQueries'
 
 // Database action for getting all maps created by a certain user
 const getUserMaps = (db:any, userid:string):Promise<Array<any>> => {
+  return new Promise((resolve:any, reject:any) => {
+    // Construct the database query
+    const query = {
+      name: 'get-user-maps',
+      text: mapQueries.getUserMaps,
+      values: [userid]
+    }
 
-    return new Promise((resolve:any, reject:any) => {
-
-        // Construct the database query
-        const query = {
-            name: 'get-user-maps',
-            text: mapQueries.getUserMaps,
-            values: [userid]
-        };
-    
-        // Perform the query
-        db.query(query)
-        .then((r:any) => {
-            if(r.rows.length === 0) {
-                reject("Invalid user id");
-            } else {
-                // Parse the JSON for the tags and ratings list stored in each map
-                r.rows.forEach((map:any) => {
-                    map.tags = JSON.parse(map.tags);
-                    map.createdOn = map.created_on;
-                    delete map.created_on;
-                    map.ratings = JSON.parse(map.ratings);
-                });
-                // Send the data back to the caller
-                resolve(r.rows);
-            }
-        })
-        .catch((err:any) => {
-            reject("Error querying database");
-        })
-    })
+    // Perform the query
+    db.query(query)
+      .then((r:any) => {
+        if (r.rows.length === 0) {
+          reject('Invalid user id')
+        } else {
+          // Parse the JSON for the tags and ratings list stored in each map
+          r.rows.forEach((map:any) => {
+            map.tags = JSON.parse(map.tags)
+            map.createdOn = map.created_on
+            delete map.created_on
+            map.ratings = JSON.parse(map.ratings)
+          })
+          // Send the data back to the caller
+          resolve(r.rows)
+        }
+      })
+      .catch((err:any) => {
+        reject('Error querying database')
+      })
+  })
 }
 
-export default getUserMaps;
+export default getUserMaps
