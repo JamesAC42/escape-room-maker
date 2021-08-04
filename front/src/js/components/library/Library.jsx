504de0cfab9f1e5a1b675d6c67d61d33d1ad4284
James Crovo <jamescrovo2019@lschs.org>
2021-08-03 21:45:07 -0400

    Add filter to library page

--- front/src/js/components/library/Library.jsx
+++ front/src/js/components/library/Library.jsx
@@ -7,17 +7,28 @@ import tagData from "../../mock-data/tagData";
 
 import search from "../../../images/search.svg";
 
+// Stores the state data for the Library page,
+// including the total list of maps, the visible maps,
+// the current search value, and the list of active tags
 class LibraryState {
   constructor() {
     this.maps = [];
+    this.visibleMaps = [];
+    this.activeTags = [];
+    this.search = "";
   }
 }
 
+// Library component class for displaying the Library page
 class Library extends Component {
   constructor(props) {
     super(props);
     this.state = new LibraryState();
   }
+
+  // Runs after the component mounts on the page, makes an
+  // API request to fetch all maps from the server and sets the
+  // state to include these maps
   componentDidMount() {
     fetch("/api/getAllMaps", {
       method: "GET",
@@ -31,6 +42,7 @@ class Library extends Component {
         if (data.success) {
           this.setState({
             maps: data.maps,
+            visibleMaps: data.maps,
           });
         }
       })
@@ -38,6 +50,85 @@ class Library extends Component {
         console.error("Error: " + error);
       });
   }
+
+  // Called when a user clicks a tag in the tag container,
+  // toggling which tags are used to filter the visible maps
+  toggleActiveTag(tag) {
+    let activeTags = [...this.state.activeTags];
+    let index = activeTags.indexOf(tag);
+    if (index === -1) {
+      activeTags.push(tag);
+    } else {
+      activeTags.splice(index, 1);
+    }
+
+    // Refilter the visible maps after toggle
+    this.setState({ activeTags }, () => {
+      this.filter();
+    });
+  }
+
+  // Event handler binding the search input to the state
+  updateSearch(e) {
+    this.setState({
+      search: e.target.value,
+    });
+  }
+
+  // Uses the search value and the active tags list to filter the visible
+  // maps
+  filter() {
+    let allMaps = [...this.state.maps];
+    let visibleMaps = [];
+
+    // Iterate over all the maps
+    allMaps.forEach((map) => {
+      const mapTags = JSON.parse(map.tags);
+
+      // Ensure the map includes all active tags being filtered for
+      let hasAllTags = true;
+      this.state.activeTags.forEach((tag) => {
+        if (mapTags.indexOf(tag) === -1) {
+          hasAllTags = false;
+        }
+      });
+
+      // Map should have all the active tags to be seen
+      if (hasAllTags) {
+        if (this.state.search !== "") {
+          // Check if the title, description, or creator includes
+          // the string being searched for
+          if (
+            map.title.includes(this.state.search) ||
+            map.description.includes(this.state.search) ||
+            map.creator.includes(this.state.search)
+          ) {
+            visibleMaps.push(map);
+          }
+        } else {
+          visibleMaps.push(map);
+        }
+      }
+    });
+
+    // Set the visible maps to be displayed
+    this.setState({ visibleMaps });
+  }
+
+  // Handles when a user presses the enter key in the
+  // search box to initiate the filters
+  handleKeyDown(e) {
+    if (e.which === 13) {
+      this.filter();
+    }
+  }
+
+  // Helper function to check if a given tag is active
+  isActive(tag) {
+    return this.state.activeTags.indexOf(tag) !== -1;
+  }
+
+  // Render the library page
   render() {
     return (
       <div className="library-container">
@@ -45,7 +136,11 @@ class Library extends Component {
           <h1 className="tag-header">Tags</h1>
           <div className="tag-section card">
             {tagData.tags.map((x) => (
-              <span key={x} className="tag-link">
+              <span
+                key={x}
+                className={"tag-link" + (this.isActive(x) ? " active" : "")}
+                onClick={() => this.toggleActiveTag(x)}
+              >
                 {x}
               </span>
             ))}
@@ -53,18 +148,28 @@ class Library extends Component {
         </div>
         <div className="whole-map-section card flex flex-col flex-center">
           <div className="search-row flex flex-row">
-            <input type="text" placeholder="Search..." className="searchbar" />
-            <div className="search-button flex center-child">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="searchbar"
+              onChange={(e) => this.updateSearch(e)}
+              onKeyDown={(e) => this.handleKeyDown(e)}
+              value={this.state.search}
+            />
+            <div
+              className="search-button flex center-child"
+              onClick={() => this.filter()}
+            >
               <img src={search} alt="Search" />
             </div>
           </div>
           <h1 style={{ textAlign: "center" }}>Popular Maps</h1>
-          {this.state.maps.map((map) => (
+          {this.state.visibleMaps.map((map) => (
             <MapItem
               key={map.uid}
               id={map.uid}
               title={map.title}
-              creator={""}
+              creator={map.creator}
               createdOn={new Date(map.createdOn)}
               rating={5}
               description={map.description}
@@ -73,6 +178,9 @@ class Library extends Component {
               ratings={JSON.parse(map.ratings)}
             />
           ))}
+          {this.state.visibleMaps.length === 0 ? (
+            <div className="no-maps">No maps!</div>
+          ) : null}
         </div>
       </div>
     );
